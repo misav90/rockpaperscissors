936bf88768fa6b9f308156ef80a41afd3bfc2c81
misav90 <savenmark.michell@gmail.com>
2021-09-16 20:37:34 +0000

    added constants to js

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+const score = document.getElementsByClassName("scores");
+const choice =document.getElementsByName("choices")
+const pchoice = document.getElementById("pchoice")
+const cchoice = document.getElementById("cchoice")
+const choiceR = document.getElementById("c-rock")
+const choiceP= document.getElementById("c-paper")
+const choiceS = document.getElementById("c-scissors")
+
+const result = docment.getElementsByClassName ("result")
