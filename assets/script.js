02c09d577fa5af9f5d01f9f56d1857424c4df1c9
misav90 <savenmark.michell@gmail.com>
2021-09-16 20:52:30 +0000

    imported fonts

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
-const score = document.getElementsByClassName("scores");
-const choice =document.getElementsByName("choices")
-const pchoice = document.getElementById("pchoice")
-const cchoice = document.getElementById("cchoice")
-const choiceR = document.getElementById("c-rock")
-const choiceP= document.getElementById("c-paper")
-const choiceS = document.getElementById("c-scissors")
 
-const result = docment.getElementsByClassName ("result")
+const score = document.getElementsByClassName("scores");
+const choice =document.getElementsByName("choices");
+const pchoice = document.getElementById("pchoice");
+const cchoice = document.getElementById("cchoice");
+const choiceR = document.getElementById("c-rock");
+const choiceP= document.getElementById("c-paper");
+const choiceS = document.getElementById("c-scissors");
+const result = document.getElementsByClassName("result");
+
